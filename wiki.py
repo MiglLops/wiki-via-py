a25cf54eee53d4edbdc3ebedbf6bf2d006ddbbcf
Miguel Lopes <migueldossantoslopes125@gmail.com>
2025-10-07 18:10:40 -0300

    Add files via upload

--- wiki.py
+++ wiki.py
@@ -1,13 +1,22 @@
 import  wikipedia
 
-
 wikipedia.set_lang("pt")
 
-assunto = wikipedia.search(input("Assunto da wiki:"))
-print(assunto)
-print("...")
-pagina = wikipedia.page(assunto[0])
-print("Conteudo Completo:"); completo = pagina.content; print(completo)
-print("*#*" * 30)
-print("Conteudo Completo:"); resumido = pagina.summary; print(resumido)
-input()
+def wiki(): #Função principal que faz a pesquisa na wiki
+    assunto = wikipedia.search(input("Assunto da wiki:"))
+    print(assunto)
+    print("...")
+    pagina = wikipedia.page(assunto[0])
+    print("Conteudo Completo:"); completo = pagina.content; print(completo)
+    print()
+    print("*#*" * 30)
+    print()
+    print("Conteudo resumido:"); resumido = pagina.summary; print(resumido)
+    nova_pesquisa = input("Deseja pesquisar novamente? S/n ")
+    if nova_pesquisa == "n":
+        exit()
+    else:
+        return
+
+while True:
+    wiki()
